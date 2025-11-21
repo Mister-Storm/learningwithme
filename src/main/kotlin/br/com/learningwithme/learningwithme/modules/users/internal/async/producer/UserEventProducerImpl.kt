b7b7dfacf8ee658d1b00615bb839946e9fe883da
mister-storm <clfernando1979@hotmail.com>
2025-11-21 20:30:32 -0300

    fix: ci file ident

--- src/main/kotlin/br/com/learningwithme/learningwithme/modules/users/internal/async/producer/UserEventProducerImpl.kt
+++ src/main/kotlin/br/com/learningwithme/learningwithme/modules/users/internal/async/producer/UserEventProducerImpl.kt
@@ -1,6 +1,7 @@
 package br.com.learningwithme.learningwithme.modules.users.internal.async.producer
 
 import arrow.core.Either
+import arrow.core.right
 import br.com.learningwithme.learningwithme.modules.users.internal.core.entity.User
 import br.com.learningwithme.learningwithme.modules.users.internal.core.errors.CreateUserError
 import br.com.learningwithme.learningwithme.modules.users.internal.core.publisher.UserEventProducer
@@ -8,15 +9,9 @@ import org.springframework.stereotype.Component
 
 @Component
 class UserEventProducerImpl : UserEventProducer {
-    override fun publishUserCreatedEvent(user: User): Either<CreateUserError, Unit> {
-        TODO("Not yet implemented")
-    }
+    override fun publishUserCreatedEvent(user: User): Either<CreateUserError, Unit> = Unit.right()
 
-    override fun publishUserUpdatedEvent(user: User): Either<CreateUserError, Unit> {
-        TODO("Not yet implemented")
-    }
+    override fun publishUserUpdatedEvent(user: User): Either<CreateUserError, Unit> = Unit.right()
 
-    override fun publishUserDeletedEvent(user: User): Either<CreateUserError, Unit> {
-        TODO("Not yet implemented")
-    }
+    override fun publishUserDeletedEvent(user: User): Either<CreateUserError, Unit> = Unit.right()
 }
