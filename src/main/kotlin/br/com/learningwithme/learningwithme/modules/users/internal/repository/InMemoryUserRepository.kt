1ba8b4e3f65c42eb4c3a1a96a19a558660f9dafa
mister-storm <clfernando1979@hotmail.com>
2025-12-13 15:25:56 -0300

    feat: implementing in memory repository

--- src/main/kotlin/br/com/learningwithme/learningwithme/modules/users/internal/repository/InMemoryUserRepository.kt
+++ src/main/kotlin/br/com/learningwithme/learningwithme/modules/users/internal/repository/InMemoryUserRepository.kt
@@ -3,17 +3,23 @@ package br.com.learningwithme.learningwithme.modules.users.internal.repository
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.raise.either
+import arrow.core.right
 import br.com.learningwithme.learningwithme.modules.users.internal.core.entity.User
 import br.com.learningwithme.learningwithme.modules.users.internal.core.entity.UserAuth
+import br.com.learningwithme.learningwithme.modules.users.internal.core.entity.UserOutbox
 import br.com.learningwithme.learningwithme.modules.users.internal.core.errors.ConfirmUserError
 import br.com.learningwithme.learningwithme.modules.users.internal.core.errors.CreateUserError
+import br.com.learningwithme.learningwithme.modules.users.internal.core.errors.OutboxError
+import br.com.learningwithme.learningwithme.modules.users.internal.core.repository.UserOutboxRepository
 import br.com.learningwithme.learningwithme.modules.users.internal.core.repository.UserRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
 import java.util.concurrent.ConcurrentHashMap
 
 @Repository
-class InMemoryUserRepository : UserRepository {
+class InMemoryUserRepository :
+    UserRepository,
+    UserOutboxRepository {
     private val users = ConcurrentHashMap<String, User>()
     private val usersByToken = ConcurrentHashMap<String, User>()
     private val authTable = ConcurrentHashMap<UUID, UserAuth>()
@@ -61,4 +67,11 @@ class InMemoryUserRepository : UserRepository {
             usersByToken[token]
                 ?: ConfirmUserError.UserNotFound.left().bind<User>()
         }
+
+    override fun save(userOutbox: UserOutbox): Either<OutboxError.PersistenceFailure, UserOutbox> = userOutbox.right()
+
+    override fun delete(userOutbox: UserOutbox): Either<OutboxError.PersistenceFailure, Unit> = Unit.right()
+
+    override fun getAllUnpublishedEvents(limit: Int): Either<OutboxError.PersistenceFailure, List<UserOutbox>> =
+        listOf<UserOutbox>().right()
 }
