4a14f0694c31daebe79c8b3d2f1f85640c229359
mister-storm <clfernando1979@hotmail.com>
2025-12-13 15:21:09 -0300

    feat: implementing outbox pattern

--- src/main/kotlin/br/com/learningwithme/learningwithme/modules/users/internal/async/producer/UserEventProducerImpl.kt
+++ src/main/kotlin/br/com/learningwithme/learningwithme/modules/users/internal/async/producer/UserEventProducerImpl.kt
@@ -1,15 +1,31 @@
 package br.com.learningwithme.learningwithme.modules.users.internal.async.producer
 
 import arrow.core.Either
+import arrow.core.raise.either
 import arrow.core.right
 import br.com.learningwithme.learningwithme.modules.users.internal.core.entity.User
+import br.com.learningwithme.learningwithme.modules.users.internal.core.entity.UserEvent
+import br.com.learningwithme.learningwithme.modules.users.internal.core.entity.UserOutbox
 import br.com.learningwithme.learningwithme.modules.users.internal.core.errors.CreateUserError
 import br.com.learningwithme.learningwithme.modules.users.internal.core.publisher.UserEventProducer
+import br.com.learningwithme.learningwithme.modules.users.internal.core.repository.UserOutboxRepository
 import org.springframework.stereotype.Component
 
 @Component
-class UserEventProducerImpl : UserEventProducer {
-    override fun publishUserCreatedEvent(user: User): Either<CreateUserError, Unit> = Unit.right()
+class UserEventProducerImpl(
+    private val repository: UserOutboxRepository,
+) : UserEventProducer {
+    override fun publishUserCreatedEvent(user: User): Either<CreateUserError, Unit> =
+        either {
+            repository
+                .save(
+                    UserOutbox(
+                        user = user,
+                        userEvent = UserEvent.CREATED,
+                    ),
+                ).mapLeft { CreateUserError.PersistenceFailure(it.reason) }
+                .bind()
+        }
 
     override fun publishUserUpdatedEvent(user: User): Either<CreateUserError, Unit> = Unit.right()
 
