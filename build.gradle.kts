159a4a73cb67831e72c667daeaa41fee0b8a0494
mister-storm <clfernando1979@hotmail.com>
2026-01-05 00:07:13 -0300

    fix: dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import java.time.Instant
 plugins {
     kotlin("jvm") version "2.2.0"
     kotlin("plugin.spring") version "2.2.0"
-    id("org.springframework.boot") version "4.0.0"
+    id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
     id("jacoco")
